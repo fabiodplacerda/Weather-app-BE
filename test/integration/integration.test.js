109c3b55a101ec33a5a16a1982ca0a74699efb99
fabiodplacerda <lacerda96@gmail.com>
2024-06-10 21:33:37 +0100

    feat: Add integration tests for /favouritecities/getcities endpoint

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -9,15 +9,22 @@ import Server from "../../src/server/Server.js";
 import FavouriteCity from "../../src/models/favouriteCity.model.js";
 import supertest from "supertest";
 import FavouriteCityController from "../../src/controller/FavouriteCity.controller.js";
+import FavouriteCityService from "../../src/services/FavouriteCity.service.js";
+
+const { testCities, newTestCity } = testFavouriteCities;
 
 describe("Integration Tests", () => {
-  let favouriteCityServer, database, request;
+  let favouriteCityServer, database, request, favouriteCityService;
 
   before(async () => {
     Config.load();
     const { PORT, HOST, DB_URI } = process.env;
+    favouriteCityService = new FavouriteCityService();
+    const favouriteCityController = new FavouriteCityController(
+      favouriteCityService
+    );
     const favouriteCityRoutes = new FavouriteCityRoutes(
-      new FavouriteCityController(),
+      favouriteCityController,
       "/favouriteCities"
     );
     favouriteCityServer = new Server(PORT, HOST, favouriteCityRoutes);
@@ -35,23 +42,56 @@ describe("Integration Tests", () => {
   beforeEach(async () => {
     try {
       await FavouriteCity.deleteMany();
-      console.log("Database cleared");
     } catch (e) {
       console.log(e.message);
       throw new Error();
     }
     try {
-      await FavouriteCity.insertMany(testFavouriteCities);
-      console.log("Database populated");
+      await FavouriteCity.insertMany(testCities);
     } catch (e) {
       console.log(e.message);
       throw new Error();
     }
   });
+
   describe("Get request to /favouritecities/getcities", () => {
     it("should respond with a 200 status code for the path /favouritecities/getcities", async () => {
       const response = await request.get("/favouritecities/getcities");
       expect(response.status).to.equal(200);
+    });
+    it("should respond with an array of favourite cities", async () => {
+      const response = await request.get("/favouritecities/getcities");
+      expect(response.body).to.be.an("array");
+    });
+    it("should respond with the right data", async () => {
+      const response = await request.get("/favouritecities/getcities");
+      const responseWithoutId = response.body.map((element) => {
+        const { __v, _id, ...elementNoId } = element;
+        return elementNoId;
+      });
+      it("should respond an empty array if there are no favourite cities", async () => {
+        await FavouriteCity.deleteMany();
+        const response = await request.get("/favouritecities/getcities");
+
+        expect(response.body).to.deep.equal([]);
+        expect(response.body).to.have.length(0);
+      });
+
+      expect(responseWithoutId).to.deep.equal(testCities);
+    });
+    it("should should respond with a 500 status code when there is an error", async () => {
+      const stub = sinon.stub(favouriteCityService, "getCities");
+      stub.throws(new Error("Test error"));
+      const response = await request.get("/favouritecities/getcities");
+
+      expect(response.status).to.equal(500);
+    });
+  });
+
+  describe("Post request to /favouritecities", () => {
+    it("should respond with a 201 status code for the path /favouritecities", async () => {
+      const response = await request.post("/favouritecities").send(newTestCity);
+      expect(response.status).to.equal(201);
     });
   });
 });
