39633299967e15d51fee789be7ce425870258252
fabiodplacerda <lacerda96@gmail.com>
2024-06-10 18:21:49 +0100

    feat: Add FavouriteCityRoutes to handle favourite city API endpoints

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import Config from "./config/Config.js";
+import FavouriteCityController from "./controller/FavouriteCity.controller.js";
 import Database from "./database/Database.js";
+import FavouriteCityRoutes from "./routes/FavouriteCity.routes.js";
 import Server from "./server/Server.js";
 
 Config.load();
-
 const { PORT, HOST, DB_URI } = process.env;
 
-const server = new Server(PORT, HOST);
+const favouriteCityRoutes = new FavouriteCityRoutes(
+  new FavouriteCityController(),
+  "/favouriteCities"
+);
+
+const server = new Server(PORT, HOST, favouriteCityRoutes);
 const database = new Database(DB_URI);
 
 server.start();
-database.connect();
+await database.connect();
