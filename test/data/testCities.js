e9db4a5dc2968261489bc3e0cc58b9e754577206
fabiodplacerda <lacerda96@gmail.com>
2024-06-10 18:23:52 +0100

    chore: Update package.json scripts for running tests and generating coverage report

---
+++ test/data/testCities.js
@@ -0,0 +1,28 @@
+const testFavouriteCities = [
+  {
+    cityName: "Lisbon",
+    cityCountry: "Portugal",
+  },
+  {
+    cityName: "Manchester",
+    cityCountry: "UK",
+  },
+  {
+    cityName: "London",
+    cityCountry: "UK",
+  },
+  {
+    cityName: "Paris",
+    cityCountry: "France",
+  },
+  {
+    cityName: "New York",
+    cityCountry: "USA",
+  },
+  {
+    cityName: "Tokyo",
+    cityCountry: "Japan",
+  },
+];
+
+export default testFavouriteCities;
